f5b2d33a19fd7249aedb5705dce6658cbe5713cb
Benaceur <Benaceur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-11 00:05:48 +0000

    tags 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/news-ticker-benaceur/tags@1109932 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.1.1/news-ticker-benaceur-page.php
@@ -0,0 +1,26 @@
+<?php 
+
+function news_ticker_benaceur_post() {
+	$ntb_st = get_option('news_ticker_benaceur_style');
+	$dir = get_option( 'news_ticker_benaceur_dir' );
+	include ('news-ticker-benaceur-panel-var.php');
+	if ($ntb_st != 'ScrollNTB') {
+	?>
+			 <div class="news-ticker-ntb">
+		 <span><?php if (!empty($ntb_title)) { echo $ntb_title; } else {echo "Latest news";} ?></span>
+	<?php
+	}
+	 $lp = new WP_Query(
+		array(
+			'post_type' => 'post',
+			'posts_per_page' => 5,
+			'cat' => get_option('news_ticker_benaceur_for_cat')
+				)
+			); 
+			if ($ntb_st == 'ScrollNTB') {
+			include ('style-anim/scrollntb.php');	
+			} else {
+			include ('style-anim/no-scrollntb.php');	
+			}
+}	
+add_action( 'wp_news_ticker_benaceur', 'news_ticker_benaceur_post' );
