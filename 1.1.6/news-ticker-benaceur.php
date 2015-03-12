02caecf9056c62ea1927ec01f96f49b0ada8a988
Benaceur <Benaceur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 17:25:08 +0000

    tags/1.1.6
    git-svn-id: https://plugins.svn.wordpress.org/news-ticker-benaceur/tags@1111264 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.1.6/news-ticker-benaceur.php
@@ -0,0 +1,239 @@
+<?php
+/** 
+Plugin Name: news ticker benaceur
+Plugin URI: http://benaceur-php.com/
+Description: This plugin allow you to display the latest news or latest articles in a bar with four beautiful animations...
+Version: 1.1.6
+Author: benaceur
+Author URI: http://benaceur-php.com/
+License: GPL2
+*/
+
+define("NEWS_TICKER_BENACEUR", "news-ticker-benaceur");
+
+// Add settings link on plugin page
+function news_ticker_benaceur_action_links($links){
+	$links[] = '<a href="'.get_admin_url(null, 'options-general.php?page='.NEWS_TICKER_BENACEUR.'').'">'.__("Settings", 'news-ticker-benaceur').'</a>';
+	return $links;
+}
+add_filter('plugin_action_links_'.plugin_basename(__FILE__), 'news_ticker_benaceur_action_links');
+// Add settings link on plugin page
+
+
+load_plugin_textdomain( 'news-ticker-benaceur', false, basename( dirname( __FILE__ ) ) . '/lang/' );
+
+add_action('admin_init', 'news_ticker_benaceur_register_options');
+
+  add_action('admin_menu', 'menu_news_ticker_benaceur');
+  function menu_news_ticker_benaceur() {
+  if (function_exists('add_options_page')) {
+  $plugin_page_options = add_options_page('news-ticker-benaceur', 'News-Ticker-Benaceur', 'administrator', 'news-ticker-benaceur', 'news_ticker_benaceur_page_options');
+  }
+}
+
+  function news_ticker_benaceur_register_options() { 
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_enable_plug');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_for_users');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_for_visitors');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_for_role_x');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_for_user_id');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_links_admin_bar_menu');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_links_admin_bar_front');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_links_admin_bar_admin');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_dir');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_style');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_for_cat');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_for_all_expt_admin');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_title');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_num_posts');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_timeout_tickerntb');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_anim_speed');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_timeout');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_speed_scrollntb');
+    register_setting('news_ticker_benaceur_group', 'news_ticker_benaceur_disable_title');
+	
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_color_back_');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_color_back_title');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_color_text_back');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_color_text_title');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_color_border');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_border_top');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_border_bottom');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_border_right');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_border_left');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_border_radius');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_opacity');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_font_family');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_font_size');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_width');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_padding_top');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_padding_bottom');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_margin_top');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_margin_bottom');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_font_weight');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_text_shadow');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_text_shadow_color');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_box_shadow');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_box_shadow_color');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_box_shadow_v');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_disable_this_font');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_padding_top_title');
+    register_setting('news_ticker_benaceur_group_sty', 'news_ticker_benaceur_default');
+	
+	    if ($GLOBALS['pagenow'] == 'options-general.php' && $_GET['page'] == 'news-ticker-benaceur'){
+		wp_enqueue_script ('jquery');
+		wp_enqueue_script('farbtastic');
+		wp_enqueue_script('news-ticker-benaceur-admin',plugins_url('admin/news-ticker-benaceur-admin.js',__FILE__), array('jquery'));
+		wp_enqueue_style('news-ticker-benaceur-admin',plugins_url('admin/news-ticker-benaceur-admin.css',__FILE__), false, '' );
+		wp_enqueue_style('farbtastic');	
+	}
+}
+
+if (get_option('news_ticker_benaceur_enable_plug')):
+
+    function wp_news_ticker_benaceur_() {  
+        do_action('wp_news_ticker_benaceur');
+       }
+	   
+//[wp_news_ticker_benaceur_short_code]
+    function shortcode_ntb_func( $atts ){
+	  return wp_news_ticker_benaceur_();
+   }
+   add_shortcode( 'wp_news_ticker_benaceur_short_code', 'shortcode_ntb_func' );
+//[wp_news_ticker_benaceur_short_code]
+
+  add_action('wp_head', 'wp_rest_news_ticker_benaceur');
+  function wp_rest_news_ticker_benaceur() { 
+  $go_ea = get_option('news_ticker_benaceur_for_all_expt_admin');
+  $admin_ntb = current_user_can( 'administrator' ); 
+  
+	if ( ( role_cap_ntb() && all_users_ntb() && id_user_ntb() && is_user_logged_in() )  || ( visitors_ntb() && !is_user_logged_in() ) || $go_ea )
+    {
+if (!$go_ea || $admin_ntb) {
+		include('news-ticker-benaceur-page.php');
+}		
+    } 
+}
+
+    function news_ticker_benaceur_scripts_() {
+	$dir = get_option( 'news_ticker_benaceur_dir' );
+	if ( !is_admin() ) {
+			wp_enqueue_script('jquery');
+		    wp_enqueue_script('jquery.anim-ntb',plugins_url('js/jquery.anim-ntb.js',__FILE__), array('jquery'));
+			if ($dir == 'ltr' || $dir == ''){
+			wp_register_script('jquery.ntb-l', plugins_url('js/jquery.ntb-l.js',__FILE__), '', '' );
+			wp_enqueue_script('jquery.ntb-l');
+			} elseif ( $dir == 'rtl') {
+			wp_register_script('jquery.ntb-r', plugins_url('js/jquery.ntb-r.js',__FILE__), '', '' );
+			wp_enqueue_script('jquery.ntb-r');
+			}	
+	}
+}
+    add_action('wp_enqueue_scripts', 'news_ticker_benaceur_scripts_');
+
+endif; // news_ticker_benaceur_enable_plug	
+
+	function get_settings_ntb( $inherit = false ) {
+
+		$settings = array( 
+			'rolexcap_ntb' => (array) get_option( 'news_ticker_benaceur_for_role_x', array() )
+		);
+
+		$settings[ 'rolexcap_ntb' ] = array_map( 'trim', array_unique( array_filter( $settings[ 'rolexcap_ntb' ] ) ) );
+		
+		return $settings;
+		
+	}
+
+    function id_user_ntb() {
+
+	$current_user = wp_get_current_user();
+	$user_id = get_current_user_id();
+	
+	$iduser_ntb = explode(',', get_option('news_ticker_benaceur_for_user_id'));
+	
+	if(is_array($iduser_ntb)) {
+		
+		foreach($iduser_ntb as $user_id) {
+			
+			if( $current_user->ID == $user_id ) {
+				
+			return false;
+			}
+		}
+	}
+	
+			return true;
+}
+
+    function role_cap_ntb() {
+
+        $settings = get_settings_ntb( true );	
+		$rolexcap_ntb = $settings[ 'rolexcap_ntb' ];
+
+		if ( !empty( $rolexcap_ntb ) ) {
+			if ( !is_array( $rolexcap_ntb ) ) {
+				$rolexcap_ntb = array( $rolexcap_ntb );
+			}
+
+			foreach ( $rolexcap_ntb as $role ) {
+			
+			if ( current_user_can( $role ) ) {
+				
+	return false;
+			}
+		}
+	}
+	
+	return true;
+}
+
+    function all_users_ntb() {
+	
+	$loggedin_ntb = get_option('news_ticker_benaceur_for_users');
+	
+	if($loggedin_ntb) {
+		
+		if(is_user_logged_in()) {
+			
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+    function visitors_ntb() {
+	
+	$visitorsntb = get_option('news_ticker_benaceur_for_visitors');
+	
+	if($visitorsntb ) {
+			
+			return false;
+	}
+	
+	return true;
+}
+
+// admin_bar
+    add_action( 'admin_bar_menu', 'ntb_links_on_admin_bar', 10155 );
+    function ntb_links_on_admin_bar($wp_admin_bar) {
+
+        if (current_user_can( 'manage_options' ) && get_option( 'news_ticker_benaceur_links_admin_bar_front') && !is_admin() && get_option( 'news_ticker_benaceur_enable_plug')) {
+if (get_option( 'news_ticker_benaceur_links_admin_bar_menu') == 'menu' || get_option( 'news_ticker_benaceur_links_admin_bar_menu') == '') {
+$wp_admin_bar->add_menu( array( 'id' => 'PLB_ntb5', 'title' => __('News Ticker Benaceur'), 'href' => admin_url('/options-general.php?page=news-ticker-benaceur' ) ) );
+} elseif (get_option( 'news_ticker_benaceur_links_admin_bar_menu') == 'submenu' ) { 
+$wp_admin_bar->add_menu( array( 'parent' => 'appearance', 'id' => 'PLB_ntb6', 'title' => __('News Ticker Benaceur'), 'href' => admin_url('/options-general.php?page=news-ticker-benaceur' ) ) );
+}
+		} elseif (current_user_can( 'manage_options' ) && get_option( 'news_ticker_benaceur_links_admin_bar_admin') && is_admin() && get_option( 'news_ticker_benaceur_enable_plug'))  {
+if (get_option( 'news_ticker_benaceur_links_admin_bar_menu') == 'menu' || get_option( 'news_ticker_benaceur_links_admin_bar_menu') == '') {
+$wp_admin_bar->add_menu( array( 'id' => 'PLB_ntb7', 'title' => __('News Ticker Benaceur'), 'href' => admin_url('/options-general.php?page=news-ticker-benaceur' ) ) );
+} elseif (get_option( 'news_ticker_benaceur_links_admin_bar_menu') == 'submenu') { 
+$wp_admin_bar->add_menu( array( 'parent' => 'site-name', 'id' => 'PLB_ntb8', 'title' => __('News Ticker Benaceur'), 'href' => admin_url('/options-general.php?page=news-ticker-benaceur' ) ) );
+}
+		}	
+}
+// admin_bar
+
+
+        require_once ('news-ticker-benaceur-panel-page.php');
---
+++ 1.1.6/style-anim/scrollntb.php
@@ -0,0 +1,134 @@
+
+				  <div class="n_t_ntb_b"><div class="n_t_ntb_b2">
+	   <?php if (!$ntb_disable_title) { ?>			  
+       <span class="n_t_ntb_b-name"><?php if (!empty($ntb_title)) { echo $ntb_title; } else {echo "Latest news";} ?></span>
+	   <?php } ?>
+        <div id="scroll-ntb">
+         <div>
+			      	<?php if ( $lp->have_posts() ) : ?>
+										<?php while ( $lp->have_posts() ) : $lp->the_post(); $do_not_duplicate[] = get_the_ID(); 
+												?>
+<img border="0" src="<?php echo '' . plugins_url( '../img/topics.gif', __FILE__ ) . ''; ?>" width="10" height="11" <?php if ($dir == 'ltr' || $dir == '')  { ?> style="margin:0 5px 0 30px;" <?php } elseif ($dir == 'rtl') { ?> style="margin:0 30px 0 5px;" <?php } ?>><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+		 <?php endwhile; endif; ?>
+         </div>
+        </div>
+      </div></div>
+      <script type="text/javascript">
+						divScrollerNTB("scroll-ntb", "ntb-hor", <?php if (!empty($ntb_speed_scrollntb)) { echo $ntb_speed_scrollntb; } else {echo "17";} ?>, 6000);
+		 			</script>
+
+<?php if ($dir == 'ltr' || $dir == '') { ?>					
+		<style>
+	.n_t_ntb_b2 { padding-right:10px;
+	 <?php if ($ntb_disable_title) {echo "padding-left: 10px;";} ?>
+	}
+	.n_t_ntb_b {
+		font-family:<?php if (!empty($ntb_font_family)) { echo $ntb_font_family; } else {echo "DroidKufi_Ben, Arial";} ?>;
+		font-size:<?php if (!empty($ntb_font_size)) { echo $ntb_font_size; } else {echo "14";} ?>px;
+		font-weight:<?php echo $ntb_font_weight ;?>;
+		background:<?php if (!empty($ntb_color_back)) { echo $ntb_color_back; } else {echo "#FFFFFF";}  ?>;
+	    border-top:<?php if (!empty($ntb_border_top)) { echo $ntb_border_top; } else {echo "0";} ?>px solid <?php if (!empty($ntb_color_border)) { echo $ntb_color_border; } else {echo "#CE1031";} ?>;
+    	border-bottom:<?php if (!empty($ntb_border_bottom)) { echo $ntb_border_bottom; } elseif ($ntb_border_bottom == '') {echo "2";} elseif ($ntb_border_bottom == '0') {echo "0";} ?>px solid <?php if (!empty($ntb_color_border)) { echo $ntb_color_border; } else {echo "#CE1031";} ?>;
+    	border-right:<?php if (!empty($ntb_border_right)) { echo $ntb_border_right; } else {echo "0";} ?>px solid <?php if (!empty($ntb_color_border)) { echo $ntb_color_border; } else {echo "#CE1031";} ?>;
+    	border-left:<?php if (!empty($ntb_border_left)) { echo $ntb_border_left; } else {echo "0";} ?>px solid <?php if (!empty($ntb_color_border)) { echo $ntb_color_border; } else {echo "#CE1031";} ?>;
+        border-radius:<?php if (!empty($ntb_border_radius)) { echo $ntb_border_radius; } elseif ($ntb_border_radius == '') {echo "1";} elseif ($ntb_border_radius == '0') {echo "0";} ?>px;
+       -moz-border-radius:<?php if (!empty($ntb_border_radius)) { echo $ntb_border_radius; } elseif ($ntb_border_radius == '') {echo "1";} elseif ($ntb_border_radius == '0') {echo "0";} ?>px;
+       -webkit-border-radius:<?php if (!empty($ntb_border_radius)) { echo $ntb_border_radius; } elseif ($ntb_border_radius == '') {echo "1";} elseif ($ntb_border_radius == '0') {echo "0";} ?>px;
+    	box-shadow:<?php echo $ntb_box_shadow; ?> <?php if (!empty($ntb_box_shadow_v)) { echo $ntb_box_shadow_v; } elseif ($ntb_box_shadow_v == '') {echo "1";} elseif ($ntb_box_shadow_v == '0') {echo "0";} ?>px <?php if (!empty($ntb_box_shadow_color)) { echo $ntb_box_shadow_color; } else {echo "#B5B5B5";} ?>;
+    	-moz-box-shadow:<?php echo $ntb_box_shadow; ?> <?php if (!empty($ntb_box_shadow_v)) { echo $ntb_box_shadow_v; } elseif ($ntb_box_shadow_v == '') {echo "1";} elseif ($ntb_box_shadow_v == '0') {echo "0";} ?>px <?php if (!empty($ntb_box_shadow_color)) { echo $ntb_box_shadow_color; } else {echo "#B5B5B5";} ?>;
+    	-webkit-box-shadow:<?php echo $ntb_box_shadow; ?> <?php if (!empty($ntb_box_shadow_v)) { echo $ntb_box_shadow_v; } elseif ($ntb_box_shadow_v == '') {echo "1";} elseif ($ntb_box_shadow_v == '0') {echo "0";} ?>px <?php if (!empty($ntb_box_shadow_color)) { echo $ntb_box_shadow_color; } else {echo "#B5B5B5";} ?>;
+    	text-shadow:<?php echo $ntb_text_shadow; ?> <?php if ($ntb_text_shadow_color) echo $ntb_text_shadow_color ; else  echo '#000000'; ?>;
+		width:<?php if (!empty($ntb_width)) { echo $ntb_width; } else {echo "100%";} ?>;
+		height:28px;
+    	margin-top:<?php if (!empty($ntb_margin_top)) { echo $ntb_margin_top; } else {echo "0";} ?>px;
+    	margin-bottom:<?php if (!empty($ntb_margin_bottom)) { echo $ntb_margin_bottom; } elseif ($ntb_margin_bottom == '') {echo "25";} elseif ($ntb_margin_bottom == '0') {echo "0";} ?>px;
+    	opacity:<?php if (!empty($ntb_opacity)) { echo $ntb_opacity; } else {echo "1";} ?>;
+		overflow:hidden;
+		position:relative;	
+		}
+	.n_t_ntb_b .n_t_ntb_b-name {
+		color:<?php if (!empty($ntb_color_text_title)) { echo $ntb_color_text_title; } else {echo "#FFFFFF";} ?>;
+		background-color:<?php if (!empty($ntb_color_back_title)) { echo $ntb_color_back_title; } else {echo "#CE0000";} ?>;
+		float:left;
+		padding:<?php if (!empty($ntb_padding_top_title)) { echo $ntb_padding_top_title; } elseif ($ntb_padding_top_title == '') {echo "2";} elseif ($ntb_padding_top_title == '0') {echo "0";} ?>px 10px 2px;
+		margin-right: 10px;
+		-moz-box-shadow: 0px 1px 3px 0 #b5b5b5;
+		-webkit-box-shadow: 0px 1px 3px 0 #B5B5B5;
+		height:28px;
+	}
+	#scroll-ntb {
+		height: 30px;
+	}
+	#scroll-ntb div {
+		padding-top:<?php if (!empty($ntb_padding_top)) { echo $ntb_padding_top; } elseif ($ntb_padding_top == '') {echo "1";} elseif ($ntb_padding_top == '0') {echo "0";} ?>px;
+        padding-bottom:<?php if (!empty($ntb_padding_bottom)) { echo $ntb_padding_bottom; } else {echo "0";} ?>;
+		margin-right: 10px;
+	}
+	#scroll-ntb a {
+		color:<?php if (!empty($ntb_color_text_back)) { echo $ntb_color_text_back; } else {echo "#000000";} ?>!important;
+		text-decoration: none!important;
+	}
+	#scroll-ntb a:hover {
+		color: #444!important;
+		text-decoration: none!important;
+		}
+		
+		
+</style>
+<?php } elseif ($dir == 'rtl') { ?>
+<style>
+	.n_t_ntb_b2 { padding-left:10px;
+	 <?php if ($ntb_disable_title) {echo "padding-right: 10px;";} ?>
+	}
+	.n_t_ntb_b {
+		font-family:<?php if (!empty($ntb_font_family)) { echo $ntb_font_family; } else {echo "DroidKufi_Ben, Arial";} ?>;
+		font-size:<?php if (!empty($ntb_font_size)) { echo $ntb_font_size; } else {echo "14";} ?>px;
+		font-weight:<?php echo $ntb_font_weight ;?>;
+		background:<?php if (!empty($ntb_color_back)) { echo $ntb_color_back; } else {echo "#FFFFFF";}  ?>;
+	    border-top:<?php if (!empty($ntb_border_top)) { echo $ntb_border_top; } else {echo "0";} ?>px solid <?php if (!empty($ntb_color_border)) { echo $ntb_color_border; } else {echo "#CE1031";} ?>;
+    	border-bottom:<?php if (!empty($ntb_border_bottom)) { echo $ntb_border_bottom; } elseif ($ntb_border_bottom == '') {echo "2";} elseif ($ntb_border_bottom == '0') {echo "0";} ?>px solid <?php if (!empty($ntb_color_border)) { echo $ntb_color_border; } else {echo "#CE1031";} ?>;
+    	border-right:<?php if (!empty($ntb_border_right)) { echo $ntb_border_right; } else {echo "0";} ?>px solid <?php if (!empty($ntb_color_border)) { echo $ntb_color_border; } else {echo "#CE1031";} ?>;
+    	border-left:<?php if (!empty($ntb_border_left)) { echo $ntb_border_left; } else {echo "0";} ?>px solid <?php if (!empty($ntb_color_border)) { echo $ntb_color_border; } else {echo "#CE1031";} ?>;
+        border-radius:<?php if (!empty($ntb_border_radius)) { echo $ntb_border_radius; } elseif ($ntb_border_radius == '') {echo "1";} elseif ($ntb_border_radius == '0') {echo "0";} ?>px;
+       -moz-border-radius:<?php if (!empty($ntb_border_radius)) { echo $ntb_border_radius; } elseif ($ntb_border_radius == '') {echo "1";} elseif ($ntb_border_radius == '0') {echo "0";} ?>px;
+       -webkit-border-radius:<?php if (!empty($ntb_border_radius)) { echo $ntb_border_radius; } elseif ($ntb_border_radius == '') {echo "1";} elseif ($ntb_border_radius == '0') {echo "0";} ?>px;
+    	box-shadow:<?php echo $ntb_box_shadow; ?> <?php if (!empty($ntb_box_shadow_v)) { echo $ntb_box_shadow_v; } elseif ($ntb_box_shadow_v == '') {echo "1";} elseif ($ntb_box_shadow_v == '0') {echo "0";} ?>px <?php if (!empty($ntb_box_shadow_color)) { echo $ntb_box_shadow_color; } else {echo "#B5B5B5";} ?>;
+    	-moz-box-shadow:<?php echo $ntb_box_shadow; ?> <?php if (!empty($ntb_box_shadow_v)) { echo $ntb_box_shadow_v; } elseif ($ntb_box_shadow_v == '') {echo "1";} elseif ($ntb_box_shadow_v == '0') {echo "0";} ?>px <?php if (!empty($ntb_box_shadow_color)) { echo $ntb_box_shadow_color; } else {echo "#B5B5B5";} ?>;
+    	-webkit-box-shadow:<?php echo $ntb_box_shadow; ?> <?php if (!empty($ntb_box_shadow_v)) { echo $ntb_box_shadow_v; } elseif ($ntb_box_shadow_v == '') {echo "1";} elseif ($ntb_box_shadow_v == '0') {echo "0";} ?>px <?php if (!empty($ntb_box_shadow_color)) { echo $ntb_box_shadow_color; } else {echo "#B5B5B5";} ?>;
+    	text-shadow:<?php echo $ntb_text_shadow; ?> <?php if ($ntb_text_shadow_color) echo $ntb_text_shadow_color ; else  echo '#000000'; ?>;
+		width:<?php if (!empty($ntb_width)) { echo $ntb_width; } else {echo "100%";} ?>;
+		height:28px;
+    	margin-top:<?php if (!empty($ntb_margin_top)) { echo $ntb_margin_top; } else {echo "0";} ?>px;
+    	margin-bottom:<?php if (!empty($ntb_margin_bottom)) { echo $ntb_margin_bottom; } elseif ($ntb_margin_bottom == '') {echo "25";} elseif ($ntb_margin_bottom == '0') {echo "0";} ?>px;
+    	opacity:<?php if (!empty($ntb_opacity)) { echo $ntb_opacity; } else {echo "1";} ?>;
+		overflow:hidden;
+		position:relative;	
+		}
+	.n_t_ntb_b .n_t_ntb_b-name {
+		color:<?php if (!empty($ntb_color_text_title)) { echo $ntb_color_text_title; } else {echo "#FFFFFF";} ?>;
+		background-color:<?php if (!empty($ntb_color_back_title)) { echo $ntb_color_back_title; } else {echo "#CE0000";} ?>;
+		float:right;
+		padding:<?php if (!empty($ntb_padding_top_title)) { echo $ntb_padding_top_title; } elseif ($ntb_padding_top_title == '') {echo "2";} elseif ($ntb_padding_top_title == '0') {echo "0";} ?>px 10px 2px;
+		margin-left: 10px;
+		-moz-box-shadow: 0px 1px 3px 0 #b5b5b5;
+		-webkit-box-shadow: 0px 1px 3px 0 #B5B5B5;
+		height:28px;
+	}
+	#scroll-ntb {
+		height: 30px;
+	}
+	#scroll-ntb div {
+		padding-top:<?php if (!empty($ntb_padding_top)) { echo $ntb_padding_top; } elseif ($ntb_padding_top == '') {echo "1";} elseif ($ntb_padding_top == '0') {echo "0";} ?>px;
+        padding-bottom:<?php if (!empty($ntb_padding_bottom)) { echo $ntb_padding_bottom; } else {echo "0";} ?>;
+		margin-left: 10px;
+	}
+	#scroll-ntb a {
+		color:<?php if (!empty($ntb_color_text_back)) { echo $ntb_color_text_back; } else {echo "#000000";} ?>!important;
+		text-decoration: none!important;
+	}
+	#scroll-ntb a:hover {
+		color: #444!important;
+		text-decoration: none!important;
+		}
+</style>
+<?php } ?>
