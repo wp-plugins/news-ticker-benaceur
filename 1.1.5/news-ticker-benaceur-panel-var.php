157fbb3bbd2a8a1a205f433ac804e7e11be8ba55
Benaceur <Benaceur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 09:56:12 +0000

    tags/1.1.5
    git-svn-id: https://plugins.svn.wordpress.org/news-ticker-benaceur/tags@1110975 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.1.5/news-ticker-benaceur-panel-var.php
@@ -0,0 +1,40 @@
+<?php
+
+$ntb_color_back = get_option( 'news_ticker_benaceur_color_back_' );
+$ntb_color_back_title = get_option( 'news_ticker_benaceur_color_back_title' );
+$ntb_color_text_back = get_option( 'news_ticker_benaceur_color_text_back' );
+$ntb_color_text_title = get_option( 'news_ticker_benaceur_color_text_title' );
+$ntb_color_border = get_option( 'news_ticker_benaceur_color_border' );
+$ntb_border_top = get_option( 'news_ticker_benaceur_border_top' );
+$ntb_border_bottom = get_option( 'news_ticker_benaceur_border_bottom' );
+$ntb_border_right = get_option( 'news_ticker_benaceur_border_right' );
+$ntb_border_left = get_option( 'news_ticker_benaceur_border_left' );
+$ntb_border_radius = get_option( 'news_ticker_benaceur_border_radius' );
+$ntb_opacity = get_option( 'news_ticker_benaceur_opacity' );
+$ntb_width = get_option( 'news_ticker_benaceur_width' );
+$ntb_padding_top = get_option( 'news_ticker_benaceur_padding_top' );
+$ntb_padding_bottom = get_option( 'news_ticker_benaceur_padding_bottom' );
+$ntb_margin_top = get_option( 'news_ticker_benaceur_margin_top' );
+$ntb_margin_bottom = get_option( 'news_ticker_benaceur_margin_bottom' );
+$ntb_font_family = get_option( 'news_ticker_benaceur_font_family' );
+$ntb_font_size = get_option( 'news_ticker_benaceur_font_size' );
+$ntb_font_weight = get_option( 'news_ticker_benaceur_font_weight' );
+$ntb_text_shadow = get_option( 'news_ticker_benaceur_text_shadow' );
+$ntb_text_shadow_color = get_option( 'news_ticker_benaceur_text_shadow_color' );
+$ntb_box_shadow = get_option( 'news_ticker_benaceur_box_shadow' );
+$ntb_box_shadow_color = get_option( 'news_ticker_benaceur_box_shadow_color' );
+$ntb_box_shadow_v = get_option( 'news_ticker_benaceur_box_shadow_v' );
+$ntb_padding_top_title = get_option( 'news_ticker_benaceur_padding_top_title' );
+$ntb_links_admin_bar_front = get_option( 'news_ticker_benaceur_links_admin_bar_front' );
+$ntb_links_admin_bar_admin = get_option( 'news_ticker_benaceur_links_admin_bar_admin' );
+$ntb_title = get_option( 'news_ticker_benaceur_title' );
+$ntb_num_posts = get_option( 'news_ticker_benaceur_num_posts' );
+$ntb_cat = get_option( 'news_ticker_benaceur_for_cat' );
+$ntb_timeout_tickerntb = get_option( 'news_ticker_benaceur_timeout_tickerntb' );
+$ntb_anim_speed = get_option( 'news_ticker_benaceur_anim_speed' );
+$ntb_timeout = get_option( 'news_ticker_benaceur_timeout' );
+$ntb_speed_scrollntb = get_option( 'news_ticker_benaceur_speed_scrollntb' );
+$ntb_disable_title = get_option( 'news_ticker_benaceur_disable_title' );
+
+
+
